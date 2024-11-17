2d1cd71603dabd7e3a5c62c5324b8ad38ea9dd60
Yuto Tanaka <karaage_nennen@icloud.com>
2024-11-17 09:29:45 +0900

    nvim-four final version

---
+++ nvim-four/lua/plugins/.disabled-plugins/ts-comments-nvim.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+local spec = {
+    "folke/ts-comments.nvim",
+    --lazy = false,
+    event = "VeryLazy",
+    opts = {},
+    cond = false,
+    enabled = false,
+}
+
+return spec
